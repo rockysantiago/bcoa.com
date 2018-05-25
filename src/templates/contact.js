e82e6109a1f9ab9cb9e847421e8fe3a930d8cd3d
Jake Dobson <jake.dobson@gmail.com>
2018-05-25 08:00:35 -0700

    contact page spacing = done

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -7,30 +7,46 @@ export default ({ data }) => {
   const contact    = data.contact;
   return (
     <div className="container">
+      <div className="marginBottom-40
+                      bp-1_marginBottom-17">
         <h1 className=" f-page-title
-                        paddingTop-7 marginBottom-7">
+                        paddingTop-7 marginBottom-7
+                        bp-1_paddingTop-11
+                        bp-2_paddingTop-17 bp-2_marginBottom-12">
           { pageFields.title }
         </h1>
         <img
           src={ pageFields.heroImage.url }
           alt={ pageFields.heroImage.alt }
         />
-        <div className="marginBottom-7"
+        <div className="marginBottom-6
+                        bp-1_marginBottom-8
+                        bp-2_marginBottom-15"
             dangerouslySetInnerHTML={{ __html: page.html }} />
+        <div className="bp-1_grid-12col
+                        bp-2_marginBottom-50">
           <p className="f-display-copy
-                        marginBottom-7">{ pageFields.message }</p>
-          <p className="f-display-copy">{ contact.address.street }</p>
-          <p className="f-display-copy">{ contact.address.street2 }</p>
-          <p className="f-display-copy">
-            { contact.address.city },{ contact.address.state } { contact.address.zip }
+                        marginBottom-7
+                        bp-1_colSpan-6">
+            { pageFields.message }
           </p>
-          <p className="f-display-copy">--</p>
-          <p>
-            <a className="f-display-copy c-red" href={`mailto:${ contact.email }`}>email</a>
-          </p>
-          <a className="f-display-copy c-red" href={`tel:${ contact.phone }`}>phone</a>
-          <p className="f-display-copy">--</p>
-          <p className="f-display-copy paddingBottom-16">Instagram</p>
+        <div className="bp-1_colSpan-6">
+            <p className="f-display-copy">{ contact.address.street }</p>
+            <p className="f-display-copy">{ contact.address.street2 }</p>
+            <p className="f-display-copy">
+              { contact.address.city },{ contact.address.state } { contact.address.zip }
+            </p>
+            <p className="f-display-copy">--</p>
+            <p>
+              <a className="f-display-copy c-red" href={`mailto:${ contact.email }`}>email</a>
+            </p>
+            <a className="f-display-copy c-red" href={`tel:${ contact.phone }`}>phone</a>
+            <p className="f-display-copy">--</p>
+            <p className="f-display-copy paddingBottom-16">Instagram</p>
+        </div>
+        </div>
+      </div>
+      <hr className="marginBottom-2"/>
     </div>
   );
 };

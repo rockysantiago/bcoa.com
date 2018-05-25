16d3e68a3301a535190b6ed75133234d8fca8de5
Josh Dobson <joshua@joshuarule.com>
2018-05-24 23:21:38 -0400

    test primary image object

--- src/cms/projects.js
+++ src/cms/projects.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
 import Hero from "../components/Hero";
+import ProjectImage from "../components/ProjectImage";
 
 export default (props) => {
   console.log(props);
   const entry = props.entry;
   const title = entry.getIn(['data', 'title']);
+  const headline = entry.getIn(['data', 'headline']);
+  const body = entry.getIn(['data', 'body']);
   const heroImg = props.getAsset(props.widgetsFor('heroImage').getIn(['data', 'url']));
-  const primaryImg = props.getAsset(props.widgetsFor('primaryImage').getIn(['data', 'image']));
-  const secondaryImg = props.getAsset(props.widgetsFor('secondaryImage').getIn(['data', 'image']));
-  const infoObjects = props.widgetsFor('infoObject');
+  const primaryImg = {
+    image: props.getAsset(props.widgetsFor('primaryImage').getIn(['data', 'image'])).toString(),
+    caption: props.widgetsFor('primaryImage').getIn(['data', 'caption']),
+    alt: props.widgetsFor('primaryImage').getIn(['data', 'alt']),
+  };
+  // const secondaryImg = props.getAsset(props.widgetsFor('secondaryImage').getIn(['data', 'image']));
+  const infoObject = props.widgetsFor('infoObject');
+  // const projectGallery = props.widgetsFor('projectGallery');
 
   return (
     <div>
@@ -20,23 +28,62 @@ export default (props) => {
           <h2 className='f-page-title'>{title}</h2>
         </div>
       </div>
-      
-      <div>{title}</div>
-      <img src={heroImg.toString()} alt="hero image" />
-      <div>{entry.getIn(['data', 'headline'])}</div>
-      <div className="marginBottom-5 bp-1_marginBottom-5 bp-2_marginBottom-10"
-        dangerouslySetInnerHTML={{ __html: entry.getIn(['data', 'body']) }}
-      />
-      {infoObjects.map((infoObject, i) => {
-        return (
-          <div key={i}>
-            <p>{infoObject.getIn(['data', 'title'])}</p>
-            <p>{infoObject.getIn(['data', 'description'])}</p>
+
+      <Hero image={heroImg.toString()} alt={'hero image'} />
+
+      <div className="container marginTop-5 bp-1_marginTop-9 bp-2_marginTop-31">
+        <div className="grid-12col">
+          <div className="colSpan-5">
+            <h1 className='f-headline-b marginBottom-4 bp-1_marginBottom-13 bp-2_marginBottom-11'>
+              {headline}-
+            </h1>
+            <div className="md marginBottom-5 bp-1_marginBottom-5 bp-2_marginBottom-10"
+              dangerouslySetInnerHTML={{ __html: body }}
+            />
+
+            {infoObject && (
+              <div className="infoObjects">
+                <dl className='grid-2col marginBottom-12 bp-1_marginBottom-16 bp-2_marginBottom-24'>
+                  {infoObject.map((item, i) => (
+                    <div key={`infoObject-${i}`} className='marginBottom-4 bp-2_marginBottom-6'>
+                      <dt className="f-credit">{infoObject.getIn(['data', 'title'])}</dt>
+                      <dd className="f-caption">{infoObject.getIn(['data', 'description'])}</dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            )}
           </div>
-        )
-      })}
-      <img src={primaryImg.toString()} alt="primary image" />
-      <img src={secondaryImg.toString()} alt="secondary image" />
+          <div className="colSpan-1"></div>
+          <div className="project-primaryImage colSpan-6">
+            <ProjectImage key='primary-image' image={primaryImg} />
+          </div>
+        </div>
+      </div>
+      {/* <div> */}
+      {/* <div className="grid-12col">
+          <ProjectImage className="colSpan-6" key='secondary-image' image={secondaryImg.toString()} />
+        </div>
+        {projectGallery &&
+          <div className="project-images grid-12col">
+            {projectGallery.map((item, i) => {
+              return (
+                projectGallery.getIn(['data', 'type']) } === 'image' ? 
+                  <ProjectImage key={i} image={item} />
+                :
+                  <blockquote key={i} className="colSpan-12 t-center f-headline-b 
+                                        marginBottom-11 bp-1_marginBottom-13 bp-2_marginBottom-28
+                                        marginTop-5 bp-1_marginTop-8 bp-2_marginTop-14">
+                    {item.pullQuote}
+                  </blockquote>
+              )
+            })}
+          </div>
+        }
+      </div> */}
+        
+        {/* <img src={primaryImg.toString()} alt="primary image" /> */}
+        {/* <img src={secondaryImg.toString()} alt="secondary image" /> */}
     </div>
   )
 };

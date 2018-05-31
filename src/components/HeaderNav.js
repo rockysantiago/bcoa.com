187ec5b356f767ddab5e32880f4d7d9d55c5dabc
Jacob Dobson <jake.dobson@gmail.com>
2018-05-31 08:34:17 -0700

    add menu animation

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,12 +1,55 @@
 import React, { Fragment } from 'react';
 import Link from "gatsby-link";
 import { icons } from "./Icons";
+import Transition from 'react-transition-group/Transition';
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+};
+
+const Menu = ({ in: inProp, menuBG,toggleMenu }) => (
+  <Transition in={ inProp } timeout={ duration }>
+    {(state) => (
+      <nav  className="menu f-navigation"
+            style={{ backgroundImage: `url("${menuBG}")`,
+                  ...defaultStyle,
+                  ...transitionStyles[state] }}
+      >
+        <ul className="c-white">
+          <li>
+            <Link to="/" onClick={toggleMenu}>Featured</Link>
+          </li>
+          <li>
+            <Link to="/work" onClick={toggleMenu}>Index</Link>
+          </li>
+          <li>
+            <Link to="/news" onClick={toggleMenu}>News</Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={toggleMenu}>About</Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    )}
+  </Transition>
+);
 
 export default ({ visible, toggleMenu, isWindowLarge, menuBackground }) => {
   console.log(menuBackground);
   return (
     <div>
-      <header className={visible ? 'c-white' : ''}>
+      <header className={visible ? 'c-white bg-transparent' : ''}>
         <div className="container">
           <div className="flex-1">
             <Link className="headerLogo" to="/">{ isWindowLarge ? icons.menuLogoLarge : icons.menuLogoSmall }</Link>
@@ -17,27 +60,7 @@ export default ({ visible, toggleMenu, isWindowLarge, menuBackground }) => {
           </button>
         </div>
       </header>
-      {visible &&
-        <nav className="menu f-navigation" style={{ backgroundImage: `url("${menuBackground}")` }}>
-          <ul className="c-white">
-            <li>
-              <Link to="/" onClick={toggleMenu}>Featured</Link>
-            </li>
-            <li>
-              <Link to="/work" onClick={toggleMenu}>Index</Link>
-            </li>
-            <li>
-              <Link to="/news" onClick={toggleMenu}>News</Link>
-            </li>
-            <li>
-              <Link to="/about" onClick={toggleMenu}>About</Link>
-            </li>
-            <li>
-              <Link to="/contact" onClick={toggleMenu}>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      }
+      <Menu in={ visible } menuBG={ menuBackground } toggleMenu={ toggleMenu } />
     </div>
   )
 }

e15c25ba33a1b528b1d6af516368d141059ef498
Josh Dobson <joshua@joshuarule.com>
2018-06-07 10:22:22 -0400

    updates cms project and image gallery

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,6 @@
 import CMS from "netlify-cms/dist/cms";
-// import { ImagesController, ImagesPreview } from "./images";
 import Projects from './projects';
 
-// CMS.registerWidget("images", ImagesController, ImagesPreview);
 CMS.registerPreviewTemplate("projects", Projects);
 CMS.registerPreviewStyle("./cms.css");
+CMS.registerPreviewStyle("./cmsOverride.css");
